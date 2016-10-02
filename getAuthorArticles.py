28c475eb78f5b339c2c70f05014bf0012b95658d
TH <hutianyou6@gmail.com>
2016-10-02 15:02:21 +1300

    update getAuthorArticles.py to collect all articles for an author

--- getAuthorArticles.py
+++ getAuthorArticles.py
@@ -9,9 +9,9 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from login import loginSA
 import json
-import codecs
-
-def getAuthor(session, url):
+from collectArticle import collectArticle
+from insertDB import insertDB 
+def getAuthorArtilesOnePage(session, url):
     baseUrl = "http://seekingalpha.com"
     userHeaders = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36"}
 
@@ -37,11 +37,31 @@ def getAuthor(session, url):
             link = link.find_all("a")[0].get("href")
             fullUrl = urljoin(baseUrl, link)
             yield fullUrl
-
-    
+def getAuthorArticles(session, authorName):
+    baseUrlPart1 =  "http://seekingalpha.com/author/"
+    baseUrlPart2 =  "/ajax_load_regular_articles?page="
+    baseUrlPart3 = "&author=true&userId=173432&sort=recent"
+    for page in range (14, 250):
+        authorUrl = baseUrlPart1 + authorName + baseUrlPart2 + str(page) + baseUrlPart3
+        articleUrls = getAuthorArtilesOnePage(session, authorUrl)
+        #print(articleUrls)
+        if articleUrls == False:
+            print("{0} has {1} pages. All done.".format(authorName, page))
+            break
+        else:
+            for articleUrl in articleUrls:
+                try:
+                    resDB = insertDB(session, articleUrl)
+                    if resDB != "success":
+                        print("Failed to insert article {0}, {1}, {2}".format(authorName, page, articleUrl))
+                    else:
+                        print("Successfully inserted article {0}, {1}, {2}".format(authorName, page, articleUrl))
+                except Exception as e:
+                    print("insertDB error, ", e)
 if __name__ == "__main__":
     url1 = "http://seekingalpha.com/author/mark-hibben/ajax_load_regular_articles?author=true&userId=6965821&sort=recent"
     url2 = "http://seekingalpha.com/author/mark-hibben/ajax_load_regular_articles?page=15&author=true&userId=6965821&sort=recent"
     url3 = "http://seekingalpha.com/author/michael-fitzsimmons/ajax_load_regular_articles?page=1&author=true&userId=173432&sort=recent"
     session = loginSA()[1]
-    getAuthor(session, url1)  
+    #getAuthorArtilesOnePage(session, url1)
+    getAuthorArticles(session, 'mark-hibben')  
